f90ef8572323687e0bee0ec51f87dfb8b9d2e5b6
ceimos <86879499+ceimos@users.noreply.github.com>
2022-12-28 16:34:14 +0530

    progress chapter 8

---
+++ chapter8/drill1/my.h
@@ -0,0 +1,6 @@
+//chapter 8 drill 1
+//my.h header file.
+
+extern int foo;
+void print_foo();
+void print(int);  
---
+++ chapter8/drill1/use.cpp
@@ -0,0 +1,15 @@
+//chapter 8 drill 1 
+//file
+
+#include "./my.h"
+#include "./my.cpp" //I shouldn't have to do this. Why is it not working without including my.cpp.
+#include <iostream>
+
+int main()
+{
+    foo=7;
+    print_foo();
+    print(99);
+    char cc;
+    std::cin>>cc;
+}
